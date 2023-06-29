a87c87c603771b7b3f365e10b5597fcec1d00c8e
jeff <ffj471011@gmail.com>
2023-06-29 20:27:06 +0800

    [Working]創建CategoriesDto跟CategoriesService

---
+++ Models/DTOs/CategoriesDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EBookStore.Site.Models.DTOs
+{
+    public class CategoriesDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
