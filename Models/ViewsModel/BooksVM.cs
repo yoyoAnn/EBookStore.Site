b9f7180fef9af6f26c56b1cd73b1b9f95d7fdd03
jeff <ffj471011@gmail.com>
2023-06-30 13:12:18 +0800

    新增BooksVM

--- Models/ViewsModel/BooksVM.cs
+++ Models/ViewsModel/BooksVM.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Xml.Linq;
 
 namespace EBookStore.Site.Models.ViewsModel
 {
@@ -9,20 +11,27 @@ namespace EBookStore.Site.Models.ViewsModel
     {
         public int Id { get; set; }
 
+        [Display(Name = "書名")]
+        [Required]
         public string Name { get; set; }
 
-        public DateTime PublishDate { get; set; }
+        public DateTime? PublishDate { get; set; }
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
 
+        [Display(Name = "ISBN")]
+        [Required]
         public string ISBN { get; set; }
-
+        [Display(Name = "EISBN")]
+        [Required]
         public string EISBN { get; set; }
 
-        public int Stock { get; set; }
+        public int? Stock { get; set; }
 
+        [Display(Name = "價格")]
+        [Required]
         public decimal Price{ get; set; }
 
-        public float Discount { get; set; }
+        public float? Discount { get; set; }
     }
 }
