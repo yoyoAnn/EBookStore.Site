529c586f247defa482cbf82ad170a43b64c40236
jeff <ffj471011@gmail.com>
2023-07-02 04:10:06 +0800

    [Working]處理excel檔案傳不進去的問題

--- Controllers/PublishersController.cs
+++ Controllers/PublishersController.cs
@@ -6,10 +6,12 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using ClosedXML.Excel;
 using EBookStore.Site.Models.DTOs;
 using EBookStore.Site.Models.EFModels;
 using EBookStore.Site.Models.Servives;
 using EBookStore.Site.Models.ViewsModel;
+using System.IO;
 
 namespace EBookStore.Site.Controllers
 {
@@ -29,6 +31,7 @@ namespace EBookStore.Site.Controllers
         {
             return View(db.Publishers.ToList());
         }
+
 
         // GET: Publishers/Details/5
         public ActionResult Details(int? id)
@@ -75,6 +78,93 @@ namespace EBookStore.Site.Controllers
 
             return View(vm);
         }
+
+
+
+        public ActionResult CreatePublishersFromExcel()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreatePublishersFromExcel(IEnumerable<HttpPostedFileBase> excelFiles)
+        {
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _services.CreatePublishersFromExcel(excelFiles);
+                    TempData["SuccessMessage"] = "從 Excel 創建出版商成功";
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", ex.Message);
+                    return View(excelFiles);
+                }
+            }
+            else
+            {
+                return View(excelFiles);
+            }
+        }
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult CreatePublishersFromExcel(PublishersVM model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            if (model.ExcelFile != null && model.ExcelFile.ContentLength > 0)
+        //            {
+        //                using (var workbook = new XLWorkbook(model.ExcelFile.InputStream))
+        //                {
+        //                    var worksheet = workbook.Worksheet(1); // 預設第一個工作表
+
+        //                    foreach (var row in worksheet.RowsUsed().Skip(1)) // 跳過標題列
+        //                    {
+        //                        var name = row.Cell(4).Value.ToString(); // 預設第四欄為出版商，讀取第四個欄位的值
+
+        //                        // 創建 Publisher 物件並設定相應的屬性
+        //                        var publisher = new PublishersVM
+        //                        {
+        //                            Id = model.Id,
+        //                            Name = name,
+        //                            Address = model.Address,
+        //                            Phone = model.Phone,
+        //                            Email = model.Email
+        //                        };
+
+        //                        // 執行創建出版商的邏輯
+        //                        _services.CreatePublisher(publisher.ToDto());
+        //                    }
+        //                }
+        //            }
+
+        //            TempData["SuccessMessage"] = "從 Excel 創建出版商成功";
+        //            return RedirectToAction("Index");
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            ModelState.AddModelError("", ex.Message);
+        //            return View(model);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        return View(model);
+        //    }
+        //}
+
+
+
+
+
+
 
         // GET: Publishers/Edit/5
         public ActionResult Edit(int? id)
--- Models/Servives/PublishersServices.cs
+++ Models/Servives/PublishersServices.cs
@@ -40,32 +40,59 @@ namespace EBookStore.Site.Models.Servives
         /// 創建出版商
         /// </summary>
         /// <param name="vms"></param>
-        public void CreatePublishersFromExcel(IEnumerable<PublishersVM> vms)
+        //public void CreatePublishersFromExcel(IEnumerable<PublishersVM> vms)
+        //{
+
+        //    foreach (var vm in vms)
+        //    {
+        //        if (!IsPublisherNameExists(vm.Name))
+        //        {
+        //            using (var workbook = new XLWorkbook(vm.ExcelFile.InputStream))
+        //            {
+        //                var worksheet = workbook.Worksheet(1);//預設第一個工作表
+
+        //                foreach (var row in worksheet.RowsUsed().Skip(1)) // 跳過標題列
+        //                {
+        //                    var name = row.Cell(4).Value.ToString(); // 預設第四欄為出版商，讀取第四個欄位的值
+
+        //                    // 創建 Publisher 物件並設定 Address 屬性
+        //                    var dto = new PublishersDto
+        //                    {
+        //                        Id = vm.Id,
+        //                        Name = name,
+        //                        Address = vm.Address,
+        //                        Phone = vm.Phone,
+        //                        Email = vm.Email
+        //                    };
+
+        //                    CreatePublisher(dto);
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+
+        public void CreatePublishersFromExcel(IEnumerable<HttpPostedFileBase> excelFiles)
         {
-            foreach (var vm in vms)
+            foreach (var excelFile in excelFiles)
             {
-                if (!IsPublisherNameExists(vm.Name))
+                using (var workbook = new XLWorkbook(excelFile.InputStream))
                 {
-                    using (var workbook = new XLWorkbook(vm.ExcelFile.InputStream))
+                    var worksheet = workbook.Worksheet(1);
+
+                    foreach (var row in worksheet.RowsUsed().Skip(1))
                     {
-                        var worksheet = workbook.Worksheet(1);//預設第一個工作表
+                        var name = row.Cell(4).Value.ToString();
 
-                        foreach (var row in worksheet.RowsUsed().Skip(1)) // 跳過標題列
+                        var dto = new PublishersDto
                         {
-                            var address = row.Cell(3).Value.ToString(); // 預設第三欄為出版商，讀取第三個欄位的值
+                            Name = name,
+                            Address = null, // Set the address, phone, and email accordingly
+                            Phone = null,
+                            Email = null
+                        };
 
-                            // 創建 Publisher 物件並設定 Address 屬性
-                            var dto = new PublishersDto
-                            {
-                                Id = vm.Id,
-                                Name = vm.Name,
-                                Address = vm.Address,
-                                Phone = vm.Phone,
-                                Email = vm.Email
-                            };
-
-                            CreatePublisher(dto);
-                        }
+                        CreatePublisher(dto);
                     }
                 }
             }
