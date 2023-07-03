aa3df1bb59ed807329ebb460ffdc701557a148d5
Felix <mimiyoyo1001@gmail.com>
2023-07-03 15:24:58 +0800

    Order Index Update

---
+++ Models/ViewModels/ProductCriteria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace EBookStore.Site.Models.ViewModels
+{
+    public class ProductCriteria
+    {
+        [Display(Name = "關鍵字搜尋")]
+        public string Name { get; set; }
+
+        [Display(Name = "訂單狀態")]
+        public int? OrderId { get; set; }
+    }
+}
