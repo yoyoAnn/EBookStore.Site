de3776a1a591b2c6583df612c37998d140030b0e
jeff <ffj471011@gmail.com>
2023-07-04 11:40:35 +0800

    調整作者頁面樣式

--- Models/Infra/DapperRepository/BookDapperRepository.cs
+++ Models/Infra/DapperRepository/BookDapperRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration.Provider;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,7 @@ namespace EBookStore.Site.Models.Infra.DapperRepository
 {
     public class BookDapperRepository
     {
+        string conntr = "data source=.;initial catalog=EBookStore;user id=ebookLogin;password=123;MultipleActiveResultSets=True;App=EntityFramework\" providerName=\"System.Data.SqlClient";
     }
 }
+
