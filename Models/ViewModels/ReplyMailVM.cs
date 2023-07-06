b5710ba87c536db538089485c7042505156e7ffa
yoyoAnn <130529691+yoyoAnn@users.noreply.github.com>
2023-07-06 14:55:21 +0800

    回覆信件存入資料庫、完成Gmail回信

--- Models/ViewModels/ReplyMailVM.cs
+++ Models/ViewModels/ReplyMailVM.cs
@@ -12,17 +12,22 @@ namespace EBookStore.Site.Models.ViewModels
 
 		public int CSId { get; set; }
 		public string Account { get; set; }
-
-		[Required]
-		[StringLength(255)]
-		public string Email { get; set; }
 		public int ProblemTypeId { get; set; }
-		[Required]
-		[StringLength(20)]
+
+		[Display(Name = "收件者")]
+		[Required(ErrorMessage = "{0} 必填")]
+		[StringLength(255)]
+		[EmailAddress(ErrorMessage = "Email 格式有誤")]
+		public string Email { get; set; }
+
+		[Display(Name = "主旨")]
+		[Required(ErrorMessage = "{0} 必填")]
+		[StringLength(20, ErrorMessage = "字數不能超過{1}")]
 		public string Title { get; set; }
 
-		[Required]
-		[StringLength(500)]
+		[Display(Name = "內容")]
+		[Required(ErrorMessage = "{0} 必填")]
+		[StringLength(500, ErrorMessage = "字數不能超過{1}")]
 		public string Content { get; set; }
 		public DateTime CreatedTime { get; set; }
 	}
