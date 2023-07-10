232d6f0af55f32013a4b999b527f42dc92589129
Felix <mimiyoyo1001@gmail.com>
2023-07-10 20:54:12 +0800

    Order_Final

---
+++ Models/Infra/DeleteOrderItemDapperRepository.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace EBookStore.Site.Models.Infra
+{
+    public class DeleteOrderItemDapperRepository
+    {
+
+        private readonly string _connStr;
+        public DeleteOrderItemDapperRepository()
+        {
+            _connStr = System.Configuration.ConfigurationManager.ConnectionStrings["AppDbContext"].ConnectionString;
+        }
+
+        public IEnumerable<DeleteOrderItemVM> DeleteOrderItem(long orderId)
+        {
+            string sql = $@"
+  delete [OrderItems]
+  where OrderId=@UserId";
+
+            IEnumerable<DeleteOrderItemVM> DetailItems = new SqlConnection(_connStr)
+                .Query<DeleteOrderItemVM>(sql, new { UserId = orderId });
+
+            return DetailItems.ToList();
+
+        }
+    }
+
+    public class DeleteOrderItemVM
+    {
+        [Display(Name = "編號")]
+        public int Id { get; set; }
+
+        [Display(Name = "名稱")]
+        public long OrderId { get; set; }
+
+        [Display(Name = "價格")]
+        public int BookId { get; set; }
+
+        [Display(Name = "個數")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "總額")]
+        public int Qty { get; set; }
+
+    }
+}
